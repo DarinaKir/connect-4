bc743dbe20df125af57a6edb644d2a0b8774aafc
DarinaKir <dasha23kir@gmail.com>
2022-12-15 00:35:14 +0200

    15/12/2022

--- src/App.js
+++ src/App.js
@@ -4,68 +4,167 @@ import React from "react";
 class App extends React.Component {
 
     state = {
-        board : [],
-        indexes : ""
+        rowsNumber: 6,
+        columnNumber: 7,
+        board: [],
+        emptyCells: [],
+        counterTurns: 0,
+        isGameOver: false,
+        isBoardFull: false
     }
 
     createNewBoard = () => {
         const rows = [];
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < this.state.rowsNumber; i++) {
             const columns = []
-            for (let j = 0; j < 6; j++) {
-                columns.push({row : j, column : i, player : 0})
+            for (let j = 0; j < this.state.columnNumber; j++) {
+                columns.push("empty")
             }
             rows.push(columns)
         }
-        // const fullIndexes = [7, 7, 7, 7, 7, 7];
+        const newEmptyCells = [];
+        for (let i = 0; i < this.state.columnNumber; i++) {
+            newEmptyCells.push(this.state.rowsNumber - 1)
+        }
         this.setState({
-            board : rows,
-            // indexes : fullIndexes
+            board: rows,
+            emptyCells: newEmptyCells
         })
-        // return rows;
     }
 
-    changePlayer = (event) => {
-        const index = event.target
-        alert(index)
-        // alert(index.row + ", " + index.column )
-        // const newBoard = this.state.board;
-        // newBoard.at(index.row).at(index.column).player = 1;
-        // this.setState({
-        //     board : newBoard
-        // })
+    componentDidMount() {
+        this.createNewBoard()
     }
 
-    // changeColor = (event) => {
-    //     const id = (event.target.id) % 10;
-    //     for (let i = 7 ; i > 0; i--) {
-    //         let cellElement  = document.getElementById(id + (i*10));
-    //         if (cellElement.classList[cellElement.classList.length-1] == "empty"){
-    //             cellElement.classList.add("player1");
-    //             break;
-    //         }
-    //     }
-    //
-    // }
+    changePlayer = (column) => {
+        const emptyCells = this.state.emptyCells[column]
+
+        if (!this.state.isGameOver && (emptyCells >= 0) && !this.state.isBoardFull){
+
+            let newBoard = this.state.board
+            let newFullCells = this.state.emptyCells
+            newFullCells[column] = this.state.emptyCells[column] - 1
+
+            let currentPlayer = "player" + ((this.state.counterTurns % 2) + 1)
+            newBoard[emptyCells][column] = currentPlayer
+
+            this.setState({
+                board: newBoard,
+                fullCells: newFullCells,
+                counterTurns: this.state.counterTurns + 1,
+                isBoardFull: ((this.state.counterTurns + 1) == (this.state.rowsNumber * this.state.columnNumber))
+            })
+
+            if (this.checkWin(column, emptyCells, currentPlayer)) {
+                this.setState({
+                    isGameOver: true
+                })
+            }
+        }
+
+    }
+
+    checkWin = (column, row, player) => {
+        return (this.checkWinInColumn(column, row, player) || this.checkWinInRow(column, row, player) ||
+            this.checkWinInDiagonalUp(column, row, player) || this.checkWinInDiagonalDown(column, row, player))
+    }
+
+    checkWinInColumn = (column, lastPaintedCell, player) => {
+        let equalCells = 1
+        if (lastPaintedCell <= (this.state.rowsNumber - 4)) {
+            for (let i = 1; i < 4; i++) {
+                if (player === this.state.board[lastPaintedCell + i][column]) {
+                    equalCells++
+                } else {
+                    break
+                }
+            }
+        }
+        return (equalCells === 4)
+    }
+
+    checkWinInRow = (column, row, player) => {
+        let equalCells = 1
+        for (let i = 1; i < (this.state.columnNumber - column - 1); i++) {
+            if (player === this.state.board[row][column + i] && (equalCells < 4)) {
+                equalCells++
+            } else {
+                break
+            }
+        }
+        for (let i = 1; i <= column; i++) {
+            if (player === this.state.board[row][column - i] && (equalCells < 4)) {
+                equalCells++
+            } else {
+                break
+            }
+        }
+        return (equalCells === 4)
+    }
+
+    checkWinInDiagonalUp = (column, row, player) => {
+        let counter = 1
+        let equalCells = 1
+        while (((column + counter) < this.state.columnNumber) && ((row - counter) >= 0) && (equalCells < 4)) {
+            if (player === this.state.board[row - counter][column + counter]) {
+                equalCells++
+            } else {
+                break
+            }
+            counter++
+        }
+        counter = 1
+        while (((column - counter) >= 0) && ((row + counter) < this.state.rowsNumber) && (equalCells < 4)) {
+            if (player === this.state.board[row + counter][column - counter]) {
+                equalCells++
+            } else {
+                break
+            }
+            counter++
+        }
+        return (equalCells === 4)
+    }
+
+    checkWinInDiagonalDown = (column, row, player) => {
+        let counter = 1
+        let equalCells = 1
+        while (((column + counter) < this.state.columnNumber) && ((row + counter) < this.state.rowsNumber) && (equalCells < 4)) {
+            if (player === this.state.board[row + counter][column + counter]) {
+                equalCells++
+            } else {
+                break
+            }
+            counter++
+        }
+        counter = 1
+        while (((column - counter) >= 0) && ((row - counter) >= 0) && (equalCells < 4)) {
+            if (player === this.state.board[row - counter][column - counter]) {
+                equalCells++
+            } else {
+                break
+            }
+            counter++
+        }
+        return (equalCells === 4)
+    }
 
 
     render() {
-        this.createNewBoard()
-
         return (
             <div className="App">
-                {/*{*/}
-                {/*    this.createNewBoard()*/}
-                {/*}*/}
+                <div className= "Headline">
+                    Connect- 4
+                </div>
                 <table>
                     {
-                        this.state.board.map((r) => {
-                        return (
+                        this.state.board.map((row) => {
+                            return (
                                 <tr>
                                     {
-                                        r.map((c) => {
+                                        row.map((cell, columnIndex) => {
                                             return (
-                                                <td onClick={this.changePlayer}>{c.player}</td>
+                                                <td className={cell}
+                                                    onClick={() => this.changePlayer(columnIndex)}></td>
                                             )
                                         })
                                     }
@@ -74,6 +173,13 @@ class App extends React.Component {
                         })
                     }
                 </table>
+
+                <br/>
+
+                <div className={"player" + (((this.state.counterTurns - 1) % 2) + 1)}>
+                    {(this.state.isGameOver) ? "Player " + (((this.state.counterTurns - 1) % 2) + 1) + " - You Are The Winner 🥇" : (this.state.isBoardFull ? "No one won ... 😕" : "")}
+                </div>
+
             </div>
         );
     }
